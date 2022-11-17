880c9b3b76eca20b811a65f9f558df994d3eae67
Mohammad Foysal <77278709+Foy5al@users.noreply.github.com>
2022-11-17 19:22:32 +0600

    gallery schema structure (#1)

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+
 const app = require("./app");
+
+//database connection
+/* mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
+  console.log(`Techno Haat Backend is connected with database successfully`);
+}); */
+
 const file = require("./test/test");
 const port = 5000;
 
