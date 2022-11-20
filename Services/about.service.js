9a8eca33c7bbfb8167961a7418112c8bad519ebc
foy5al <mh.foysal.h@gmail.com>
2022-11-20 18:12:03 +0600

    schema processing

--- Services/about.service.js
+++ Services/about.service.js
@@ -1,7 +1,12 @@
 //operation = RU
 const aboutInfoSchema = require("../Models/AboutInfo");
 
+exports.createAboutInfoSchema = async (data) => {
+  const result = await aboutInfoSchema.create(data);
+  return result;
+};
+
 exports.getAboutInfoSchema = async () => {
-  const result = await aboutInfoSchema.find();
+  const result = await about.find();
   return result;
 };
