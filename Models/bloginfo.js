e4824df64d9cc110a20c08eb8b622d8a8a8db929
Mohammad Foysal <77278709+Foy5al@users.noreply.github.com>
2022-11-20 14:13:10 +0600

    About section (#2)
    * gallery schema structure
    
    * contact schema

---
+++ Models/bloginfo.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const blogInfoSchema = mongoose.Schema({
+  //pending have to update it according to the dashboard
+});
