ec8574a579b69b89292bf2f69b73271f1ad0b289
foy5al <mh.foysal.h@gmail.com>
2022-11-23 19:30:07 +0600

    mongo fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const colors = require("colors");
 const app = require("./app");
 
 //database connection
-mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
+mongoose.connect(process.env.MONGODB_URI).then(() => {
   console.log(
     `Techno Haat Backend is connected with database successfully`.cyan.bold
   );
