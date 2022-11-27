302f9228032f60039704aadcce63b215faa23faf
foy5al <mh.foysal.h@gmail.com>
2022-11-27 12:07:39 +0600

    footer update

--- Services/footer.service.js
+++ Services/footer.service.js
@@ -1,1 +1,16 @@
-//operation = RU
+//operation = CRD
+const footerInfo = require("../Models/footerinfo");
+
+exports.createFooterInfoSchema = async (data) => {
+  const result = await footerInfo.create(data);
+  return result;
+};
+
+exports.getFooterInfoSchema = async () => {
+  const result = await footerInfo.find();
+  return result;
+};
+exports.updateFooterInfoSchema = async (data) => {
+  const result = await footerInfo.updateMany(data);
+  return result;
+};
