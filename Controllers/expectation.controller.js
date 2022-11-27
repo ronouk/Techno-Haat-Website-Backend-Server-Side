df8c9b8bbf679a9812c2e0366c61e2905f04679b
foy5al <mh.foysal.h@gmail.com>
2022-11-27 15:00:02 +0600

    expt done

---
+++ Controllers/expectation.controller.js
@@ -0,0 +1,61 @@
+//operation = CRUD
+
+const {
+  createExpectationInfoSchema,
+  getExpectationInfoSchema,
+  updateExpectationInfoSchema,
+} = require("../Services/Expectation.service");
+
+exports.createExpectation = async (req, res, next) => {
+  try {
+    const result = await createExpectationInfoSchema(req.body);
+
+    res.status(200).json({
+      status: "success",
+      message: "Expectation Data inserted Successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      message: "Expectation data couldn't insert an error occeured",
+      error: error.message,
+    });
+  }
+};
+
+exports.getExpectation = async (req, res, next) => {
+  try {
+    const result = await getExpectationInfoSchema(req.body);
+
+    res.status(200).json({
+      status: "success",
+      message: "Expectation Data get Successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      message: "Expectation data get error",
+      error: error.message,
+    });
+  }
+};
+
+exports.updateExpectation = async (req, res, next) => {
+  try {
+    const result = await updateExpectationInfoSchema(req.body);
+
+    res.status(200).json({
+      status: "success",
+      message: "Expectation Data Updated Successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      message: "Expectation couldn't update an error occurred",
+      error: error.message,
+    });
+  }
+};
