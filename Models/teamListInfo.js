56e692eb97936121f7387182ad36513a0ab8faf0
foy5al <mh.foysal.h@gmail.com>
2022-12-03 14:46:37 +0600

    team...

--- Models/teamListInfo.js
+++ Models/teamListInfo.js
@@ -1,19 +1,24 @@
 //DB operation ru
 const mongoose = require("mongoose");
 
-const teamListInfoSchema = mongoose.Schema({
-  name: String,
-  image: String,
-  designation: String,
-  social: [
-    [
-      {
-        type: String,
-      },
+const teamListInfoSchema = mongoose.Schema(
+  {
+    name: String,
+    image: String,
+    designation: String,
+    social: [
+      [
+        {
+          type: String,
+        },
+      ],
     ],
-  ],
-  description: String,
-});
+    description: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const teamListInfo = mongoose.model("teamList", teamListInfoSchema);
 
